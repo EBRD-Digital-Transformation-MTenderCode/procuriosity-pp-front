feabb353e357ceea030ffbe71016bc73c5f990c3
annisokay <annisokay97@gmail.com>
2018-10-17 17:51:57 +0300

    add ro lang for static pages

--- src/views/StaticPages/ForCivil.vue
+++ src/views/StaticPages/ForCivil.vue
@@ -3,54 +3,54 @@
     <div class="static">
       <aside class="static-aside">
         <div class="static-aside__title">
-          For Civil Society
+          {{$t("message.for_civil_title")}}
         </div>
       </aside>
       <section class="static-content">
-        <h1>Open Data, Open Access, Open Government</h1>
-        <p>In market economy competition, equal access and transparent procurement procedures result in lower prices and higher quality and provide better quality public services. With high pressure on governments to efficiently use taxpayers’ money for delivering public services reducing the cost of public spending is a key aspect of public procurement. It is also important that procurement officers responsible for purchasing goods, services and works for public sector respect taxpayers’ money and execute their duties in such a way that citizens can trust their professionalism and honesty in spending public funds.</p>
-        <p>Civil Society has the power to influence political, economic or public interest decisions, serving to monitor the work of government and public institutions and to take an attitude towards policies that are contrary to the citizen interests or in case of power abuse.</p>
-        <p>Every citizen and civil society organization shall be given an unrestricted access to public procurement information. Transparency of public procurement information can be easily and inexpensively achieved by implementing digital public procurement systems that by default use Open Data to provide an open access to public procurement information.</p>
-        <p>Building on global best practice, the MTender System is developed to satisfy <a href="https://www.open-contracting.org/" target="_blank">Advanced Open Contracting Data Standards</a> and enables open access to public procurement information to all citizens of Moldova and local Civil Society organizations.</p>
-        <p>Thanks to this approach, every citizen is able to use information provided by the MTender System to monitor how government is undertaking public duties and whether principles of fair competition and value for money in public procurement are respected in Moldova.</p>
-        <p>Upon its completion, the MTender System will provide the Civil Society with dedicated monitoring portal to provide feedback to Contracting Authorities, discuss specific procurements, assess quality of public contract delivery and submit a review request to enforcement bodies when necessary.</p>
-        <p><span class="bold">Recognizing the value of electronic procurement for civil society in Moldova, piloting of the MTender System is supported by the following non-governmental organizations:</span></p>
+        <h1>{{$t("message.for_civil_h11")}}</h1>
+        <p>{{$t("message.for_civil_p1")}}</p>
+        <p>{{$t("message.for_civil_p2")}}</p>
+        <p>{{$t("message.for_civil_p3")}}</p>
+        <p>{{$t("message.for_civil_p4_before")}}<a href="https://www.open-contracting.org/" target="_blank">{{$t("message.for_civil_p4_bold")}}</a>{{$t("message.for_civil_p4_after")}}</p>
+        <p>{{$t("message.for_civil_p5")}}</p>
+        <p>{{$t("message.for_civil_p6")}}</p>
+        <p><span class="bold">{{$t("message.for_civil_p7")}}</span></p>
         <div class="organizations">
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/1.png" alt="organization 1">
             </div>
-            <div class="organizations-item__name">Centrul Analitic Independent „Expert-Grup”</div>
+            <div class="organizations-item__name"><p>{{$t("message.for_civil_organization1")}}</p></div>
           </div>
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/2.png" alt="organization 2">
             </div>
-            <div class="organizations-item__name">IDIS “Viitorul”</div>
+            <div class="organizations-item__name">{{$t("message.for_civil_organization2")}}</div>
           </div>
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/3.png" alt="organization 1">
             </div>
-            <div class="organizations-item__name">Asociaţia pentru Guvernare Eficientă şi Responsabilă (AGER)</div>
+            <div class="organizations-item__name">{{$t("message.for_civil_organization3")}}</div>
           </div>
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/4.png" alt="organization 1">
             </div>
-            <div class="organizations-item__name">Agenţia pentru Dezvoltare Regională “Habitat”</div>
+            <div class="organizations-item__name">{{$t("message.for_civil_organization4")}}</div>
           </div>
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/5.png" alt="organization 1">
             </div>
-            <div class="organizations-item__name">Transparency International-Moldova</div>
+            <div class="organizations-item__name">{{$t("message.for_civil_organization5")}}</div>
           </div>
           <div class="organizations-item">
             <div class="organizations-item__img">
               <img src="@/assets/organizations/6.png" alt="organization 1">
             </div>
-            <div class="organizations-item__name">Initiativa Pozitiza</div>
+            <div class="organizations-item__name">{{$t("message.for_civil_organization6")}}</div>
           </div>
         </div>
       </section>
--- src/views/StaticPages/JoinMTender.vue
+++ src/views/StaticPages/JoinMTender.vue
@@ -3,27 +3,27 @@
     <div class="static">
       <aside class="static-aside">
         <div class="static-aside__title">
-          Join the MTender
+          {{$t("message.join_title")}}
         </div>
       </aside>
       <section class="static-content">
-        <h1>MTender is open to collaboration</h1>
-        <p>The MTender service is a partnership between the Ministry of Finance and commercial electronic platforms formed to provide collaboratively digital procurement services to public sector and commercial buyers in Moldova.</p>
-        <p>The MTender service supports electronic tendering and online contracts. In the (near) future it will offer an online shopping and secure online payments. MTender is also a community of stakeholders – it connects business, the civil society and the government within cooperative work to deliver quality public services to citizens of Moldova.</p>
-        <h3>Become a buyer</h3>
-        <p>The MTender digital service is available free of charge to public sector and commercial buyers in Moldova.</p>
+        <h1>{{$t("message.join_h11")}}</h1>
+        <p>{{$t("message.join_p1")}}</p>
+        <p>{{$t("message.join_p2")}}</p>
+        <h3>{{$t("message.join_h31")}}</h3>
+        <p>{{$t("message.join_p3")}}</p>
         <ul>
-          <li>Commercial buyers may use the MTender services for any type of purchase and when they want to sign an online contract.</li>
-          <li>public sector entities referred to in the law on public procurement starting from <span class="bold">1 October 2018</span> are required to use the MTender to conduct electronic public procurement and sign electronic contracts.</li>
+          <li>{{$t("message.join_ul1li1")}}</li>
+          <li>{{$t("message.join_ul1li2_before")}}<span class="bold">{{$t("message.join_ul1li2_bold")}}</span>{{$t("message.join_ul1li2_after")}}</li>
         </ul>
-        <h3>Become a supplier</h3>
-        <p>It is easy to bid online! Register on the MTender and bid for contracts. Instruction how to register is provided by each MTender networking platform.</p>
-        <p>Any economic operator who successfully registered on the MTender and accepted the terms of use is eligible to bid in any commercial online tender. </p>
-        <p>To bid for public contracts the economic operators are required to register on the MTender, accept the terms of use and mandatory minimum requirements for economic operators</p>
-        <h3>Become an MTender networking platform</h3>
-        <p>The MTender is open to new partners! Any interested e-commerce operator should contact the Ministry of Finance, find out about digital service quality and security requirements and complete the certification process to offer digital procurement services on the MTender.</p>
-        <h3>Become a MTender watchdog</h3>
-        <p>Passionate about quality of public services? Want to understand how government spends money and monitor quality of public services purchased by the government? Join the MTender civil society network and become the MTender watchdog. The European Union Delegation to Moldova, the EBRD Civil Society Unit, the UNDP and the World Bank offer regular grants to support civil society initiatives.</p>
+        <h3>{{$t("message.join_h32")}}</h3>
+        <p>{{$t("message.join_p4")}}</p>
+        <p>{{$t("message.join_p5")}}</p>
+        <p>{{$t("message.join_p6")}}</p>
+        <h3>{{$t("message.join_h33")}}</h3>
+        <p>{{$t("message.join_p7")}}</p>
+        <h3>{{$t("message.join_h34")}}</h3>
+        <p>{{$t("message.join_p8")}}</p>
       </section>
     </div>
   </el-container>
