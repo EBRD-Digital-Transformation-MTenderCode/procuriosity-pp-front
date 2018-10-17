7b8f1afef647d6fbdc83e7f71824f7abe7e8625a
annisokay <annisokay97@gmail.com>
2018-10-17 18:48:54 +0300

    add ro lang for main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -4,37 +4,37 @@
       <el-container direction="vertical">
         <div class="main-title">
           <div>MTender</div>
-          Leading by example
+          {{$t("message.main_title")}}
         </div>
         <div class="main-count">
           <div class="main-count__number">12′25′413 <span class="main-count_currency">MDL</span></div>
-          <div class="main-count__text">Total savings</div>
+          <div class="main-count__text">{{$t("message.main_total_saving")}}</div>
         </div>
-        <a href="/" class="main__join-button">Join</a>
+        <a href="/" class="main__join-button">{{$t("message.main_join")}}</a>
       </el-container>
     </div>
     <el-container direction="vertical">
       <div class="main-news">
         <div class="main-news__title">
-          News
+          {{$t("message.main_news")}}
         </div>
         <div class="main-news__cards-wp">
           <div class="main-news__card">
             <img src="@/assets/news1.jpg" alt="news1" class="main-news__img">
             <div class="main-news__text">
-              New types of electronic tendering procedures are becoming available on the MTender, to suit different types of contracts and serve different public and commercial buyers, including state-owned enterprises. The Ministry of Finance of Moldova is first introducing electronic public procurement procedures of open tender, request for quotation, and negotiated procedure without publication of contract notice.
+              {{$t("message.main_news1")}}
             </div>
           </div>
           <div class="main-news__card">
             <img src="@/assets/news2.jpg" alt="news2" class="main-news__img">
             <div class="main-news__text">
-              On 22-26 October 2018, the Ministry of Finance of Moldova and the Speaker of the Parliament of Moldova are hosting the WTO Regional Seminar for GPA Parties and Observer Governments from Central Europe, Western Balkans, Caucasus and Central Asia, including a session on digital public procurement in Moldova. Register to attend the UNDP Open Government and Public Spending session on 22 October.
+              {{$t("message.main_news2")}}
             </div>
           </div>
           <div class="main-news__card">
             <img src="@/assets/news3.jpg" alt="news3" class="main-news__img">
             <div class="main-news__text">
-              From 26 October 2018, competitive and transparent but easy-to-use online bidding procedures, with lowest price and price and non-price selection criteria, will be offered to interested state-owned enterprises and commercial clients. State-owned enterprises and commercial clients may also use the MTender online contract signing service.
+              {{$t("message.main_news2")}}
             </div>
           </div>
         </div>
@@ -43,42 +43,41 @@
     <div class="main-about">
       <el-container direction="vertical">
         <div class="main-about__title">
-          What is M-Tender
+          {{$t("message.main_h11")}}
         </div>
         <div class="main-about__text">
-          The  Ministry of Finance of the Republic of Moldova is leading a transition to digital public procurement to ensure more transparent and efficient spending of the state budget of the Republic of Moldova. New digital government service – MTender - will support public
-          procurement from planning the purchase to payment for public contracts. It will shorten time for tendering for public bodies and waiting time for payment for suppliers and contractors.
+          {{$t("message.main_p1")}}
         </div>
         <div class="main-about__cards-wp">
           <div class="main-about__card">
             <img src="@/assets/main-ico1.png" alt="main ico 1" class="main-about__img">
             <div class="main-about__card-text">
-              With MTender digital service, all public tenders are accessible online and every procurement decision transparently published online in real time. Citizens can watch government procurement decisions and how taxpayers money are spent on public services.
+              {{$t("message.main_ul1_li1")}}
             </div>
           </div>
           <div class="main-about__card">
             <img src="@/assets/main-ico2.png" alt="main ico 2" class="main-about__img">
             <div class="main-about__card-text">
-              On account of online and easy to participate tenders, the new fully digital procurement service will offer better access to opportunities in public procurement for business community, in particular local small and medium-sized enterprises.
+              {{$t("message.main_ul1_li2")}}
             </div>
           </div>
           <div class="main-about__card">
             <img src="@/assets/main-ico3.png" alt="main ico 3" class="main-about__img">
             <div class="main-about__card-text">
-              For public bodies, new digital service will also bring improved planning of spending, quicker purchases and better-quality as a result of ‘zero paper’ electronic bidding procedures.
+              {{$t("message.main_ul1_li3")}}
             </div>
           </div>
         </div>
         <hr class="main-about__line">
         <div class="main-about__sub-title">
-          The MTender is a multi-platform networking digital procurement service, consisting of  a government-operated web portal and Open Data central database unit and networking with several commercial electronic platforms certified to support electronic tendering procedures for public sector and commercial clients
+          {{$t("message.main_p2")}}
         </div>
         <div class="main-about__sub-text">
-          The MTender networking electronic procurement platform is a commercial electronic procurement platform, authorised and certified for networking with the MTender and providing digital electronic tendering services to public sector clients as well as commercial buyers. Each MTender networking platform is providing a help desk service to buyers and economic operators registered on this platform to access digital services of the MTender.
+          {{$t("message.main_p3")}}
         </div>
         <div class="main-about__partners">
           <div class="main-about__partners_title">
-            Presently the MTender is networking with the following platforms:
+            {{$t("message.main_p4")}}
           </div>
           <div class="main-about__partners_links">
             <a href="https://yptender.md/" target="_blank">
