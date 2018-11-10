9463eb4b4841f5159bd8c61d876bbbff9b4427b7
Dima Volkov <mr.rotberry@gmail.com>
2018-11-10 18:27:32 +0200

    add check URL for redirect from old link to new link

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,16 @@
         const lang = window.location.pathname.match(/\/en\/|\/ru\//)[0].replace(/\//g, "");
         this.$i18n.locale = lang;
       }
+
+      if ((/^#\/tenders\/ocds-([a-z]|[0-9]){6}-[A-Z]{2,}-[0-9]{13}$/).test(window.location.hash) ||
+        (/^#\/tenders\/MD-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}-[0-9]{1}$/).test(window.location.hash)) {
+        this.$router.replace(window.location.hash.replace("#", ""))
+      }
+
+      if ((/^#\/contracts\/ocds-([a-z]|[0-9]){6}-[A-Z]{2,}-[0-9]{13}$/).test(window.location.hash) ||
+        (/^#\/contracts\/MD-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}-[0-9]{1}-[0-9]{2}$/).test(window.location.hash)) {
+        this.$router.replace(window.location.hash.replace("#", ""))
+      }
     }
   };
 </script>
