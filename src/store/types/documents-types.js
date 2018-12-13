1490424d93734ff53dc293331ec6e46419d03ec1
annisokay <annisokay97@gmail.com>
2018-12-13 19:33:33 +0200

    add doc types

---
+++ src/store/types/documents-types.js
@@ -0,0 +1,152 @@
+export default {
+  "tenderNotice": {
+    en: "Contract Notice",
+    ro: "Invitaţie de participare",
+    ru: "Приглашение на участие"
+  },
+  "biddingDocuments": {
+    en: "Tender Documents",
+    ro: "Documentaţie de atribuire",
+    ru: "Документация по присуждению"
+  },
+  "technicalSpecifications": {
+    en: "Technical Specifications",
+    ro: "Specificații tehnice",
+    ru: "Технические спецификации"
+  },
+  "evaluationCriteria": {
+    en: "Evaluation Criteria",
+    ro: "Criterii de evaluare",
+    ru: "Критерии оценки"
+  },
+  "clarifications": {
+    en: "Clarification of Tender Documents",
+    ro: "Clarificări pentru participanți",
+    ru: "Разъяснения для участников"
+  },
+  "eligibilityCriteria": {
+    en: "Eligibility Criteria",
+    ro: "Criterii de calificare",
+    ru: "Критерии приемлимости"
+  },
+  "shortlistedFirms": {
+    en: "Shortlisted Firms",
+    ro: "Lista OE preselectaţi",
+    ru: "Список предварительного отбора"
+  },
+  "riskProvisions": {
+    en: "Provisions for management of risks and liabilities",
+    ro: "Dispoziții privind gestionarea riscurilor și obligațiilor",
+    ru: "Положения по управлению рисками и обязательств"
+  },
+  "billOfQuantity": {
+    en: "Bill Of Quantity",
+    ro: "Deviz de cheltuieli",
+    ru: "Смета расходов"
+  },
+  "conflictOfInterest": {
+    en: "Identified conflicts of interest uncovered",
+    ro: "Conflicte de interese descoperite",
+    ru: "Обнаруженные конфликты интересов"
+  },
+  "procurementPlan": { //TODO add translates
+    en: "",
+    ro: "",
+    ru: ""
+  },
+  "evaluationReports": {
+    en: "Evaluation report",
+    ro: "Raport de evaluare",
+    ru: "Отчет об оценке"
+  },
+  "contractDraft": {
+    en: "Draft contract - special conditions",
+    ro: "Model de сontract",
+    ru: "Проект договора"
+  },
+  "contractArrangements": {
+    en: "Arrangements for closing or termination of the contract",
+    ro: "Aranjamente de încetare a contractului",
+    ru: "Условия прекращения договора"
+  },
+  "complaints": {
+    en: "Complaints and review decisions",
+    ro: "Reclamații și decizii",
+    ru: "Жалобы и решения"
+  },
+  "contractGuarantees": {
+    en: "Contract Performance Guarantees",
+    ro: "Garanții",
+    ru: "Гарантии исполнения договора"
+  },
+  "illustration": {//TODO add translates
+    en: "",
+    ro: "",
+    ru: ""
+  },
+  "cancellationDetails": {//TODO add translates
+    en: "",
+    ro: "",
+    ru: ""
+  },
+  "awardNotice": {
+    en: "Award Notification",
+    ro: "Anunț de atribuire",
+    ru: "Oбъявление о присуждении"
+  },
+  "winningBid": {
+    en: "Winning Bid",
+    ro: "Oferta câștigătoare",
+    ru: "Выигравшее предложение"
+  },
+  "contractSchedule": {
+    en: "Schedules and milestones",
+    ro: "Etape planificate",
+    ru: "График и запланированные этапы"
+  },
+  "contractNotice": {
+    en: "Contract Award Notice",
+    ro: "Anunț de contractare",
+    ru: "Уведомление о договоре"
+  },
+  "contractSigned": {
+    en: "Signed Contract",
+    ro: "Contract semnat",
+    ru: "Подписанный договор"
+  },
+  "contractAnnexe": {
+    en: "Annexes to the Contract",
+    ro: "Anexe la contract",
+    ru: "Приложения к договору"
+  },
+  "subContract": {
+    en: "Subcontracts",
+    ro: "Subcontracte",
+    ru: "Субконтракты"
+  },
+  "contractSummary": {//TODO add translates
+    en: "",
+    ro: "",
+    ru: ""
+  },
+  "x_commercialOffer": {
+    en: "Financial proposal",
+    ro: "Propunere financiară",
+    ru: "Financial proposal"//TODO add translates
+  },
+  "x_qualificationDocuments": {
+    en: "Qualification documents",
+    ro: "Documente de calificare",
+    ru: "Квалификационные документы"
+  },
+  "x_eligibilityDocuments": {
+    en: "Eligibility documents",
+    ro: "Documente de eligibilitate",
+    ru: "Документы соответствия"
+  },
+  "submissionDocuments": {//TODO add translates
+    en: "",
+    ro: "",
+    ru: ""
+  },
+};
