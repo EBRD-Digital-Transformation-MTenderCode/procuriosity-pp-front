c54c344ecc480e156ce01f9ad16003008588eb65
annisokay <annisokay97@gmail.com>
2019-03-11 14:21:48 +0200

    code formating

--- src/i18n/messages/ro/budgetBreakdown.js
+++ src/i18n/messages/ro/budgetBreakdown.js
@@ -1,14 +1,13 @@
 export default {
-  "procuring_entity_identifier": "Număr naţional de înregistrare",
-  "budgetline_id": "IBAN",
-  "amount": "Valoarea",
-  "budget_details_rationale": "Detalii despre buget sau argumentarea finanțării",
-  "validity_period": "Perioada de disponibilitate",
-  "end_date": "până la",
-  "budget_project": "Proiectul bugetar",
-  "project_ID": "ID-ul proiectui",
-  "budget_owner": "Titularul Bugetului",
-  "payer_entity": "Entitate plătitoare",
-  "funding_entity": "Entitatea finanțatoare",
-
-}
+  procuring_entity_identifier: "Număr naţional de înregistrare",
+  budgetline_id: "IBAN",
+  amount: "Valoarea",
+  budget_details_rationale: "Detalii despre buget sau argumentarea finanțării",
+  validity_period: "Perioada de disponibilitate",
+  end_date: "până la",
+  budget_project: "Proiectul bugetar",
+  project_ID: "ID-ul proiectui",
+  budget_owner: "Titularul Bugetului",
+  payer_entity: "Entitate plătitoare",
+  funding_entity: "Entitatea finanțatoare",
+};
