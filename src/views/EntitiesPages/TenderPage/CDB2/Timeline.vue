e0673c217be054e31ab7eedf29e7a2360799830a
annisokay <annisokay97@gmail.com>
2019-07-18 16:24:42 +0300

    add new timeline conception

--- src/views/EntitiesPages/TenderPage/CDB2/Timeline.vue
+++ src/views/EntitiesPages/TenderPage/CDB2/Timeline.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="timeline">
-    <div :class="[{ 'timeline__item--active': getActivePeriod === 'clarifications' }, 'timeline__item']">
+  <div :class="[{ 'timeline--cancelled': getActivePeriod === 'cancelled' }, 'timeline']">
+    <div
+      :class="[
+        { 'timeline__item--active': getActivePeriod === 'clarification' || getActivePeriod === 'suspended' },
+        'timeline__item',
+      ]"
+    >
       <span class="timeline__date">{{ fd(periods.enquiryPeriodStart) }}</span>
+      <span
+        :class="[
+          'timeline__marker',
+          getActivePeriod === 'suspended' ? 'timeline__marker--suspended' : 'timeline__marker--clarification',
+        ]"
+      ></span>
       <div class="timeline__period">Clarifications</div>
     </div>
     <div :class="[{ 'timeline__item--active': getActivePeriod === 'tendering' }, 'timeline__item']">
       <span class="timeline__date">{{ fd(periods.enquiryPeriodEnd) }}</span>
+      <span class="timeline__marker timeline__marker--tendering"></span>
       <div class="timeline__period">Tendering</div>
     </div>
     <div
@@ -13,14 +25,34 @@
       v-if="periods.auctionPeriodStart"
     >
       <span class="timeline__date">{{ fd(periods.auctionPeriodStart) }}</span>
+      <span class="timeline__marker timeline__marker--auction"></span>
       <div class="timeline__period">Auction</div>
     </div>
     <div :class="[{ 'timeline__item--active': getActivePeriod === 'awarding' }, 'timeline__item']">
       <span class="timeline__date">{{ fd(periods.awardPeriodStart) }}</span>
+      <span class="timeline__marker timeline__marker--awarding"></span>
       <div class="timeline__period">Awarding</div>
     </div>
-    <div :class="[{ 'timeline__item--active': getActivePeriod === 'awarded' }, 'timeline__item']">
+    <div
+      :class="[
+        {
+          'timeline__item--active':
+            getActivePeriod === 'awarded' || getActivePeriod === 'unsuccessful' || getActivePeriod === 'cancelled',
+        },
+        'timeline__item',
+      ]"
+    >
       <div class="timeline__date">{{ fd(periods.awardPeriodEnd) }}</div>
+      <span
+        :class="[
+          'timeline__marker',
+          getActivePeriod === 'awarded'
+            ? 'timeline__marker--awarded'
+            : getActivePeriod === 'unsuccessful'
+            ? 'timeline__marker--unsuccessful'
+            : 'timeline__marker--cancelled',
+        ]"
+      ></span>
     </div>
   </div>
 </template>
@@ -49,25 +81,25 @@ export default {
       const statusFull = `${this.status}.${this.statusDetails}`;
       switch (statusFull) {
         case "active.clarification":
-        case "active.enquiries":
+          return "clarification";
         case "active.suspended":
-          return "clarifications";
+          return "suspended";
         case "active.tendering":
           return "tendering";
         case "active.auction":
           return "auction";
         case "unsuccessful.empty":
-        case "unsuccessful":
+          return "unsuccessful";
         case "active.awardedContractPreparation":
-        case "active.awarded":
           return "awarded";
         case "active.awarding":
-        case "active.qualification":
           return "awarding";
         case "cancelled.empty":
+          return "cancelled";
       }
     },
   },
+
   methods: {
     gd(...args) {
       return getDataFromObject(...args);
@@ -79,6 +111,8 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+@import "./../../../../styles/mixins";
+
 .timeline {
   display: flex;
   justify-content: space-between;
@@ -93,12 +127,12 @@ export default {
     position: absolute;
     width: 100%;
     height: 1px;
-    transform: translateY(26px);
+    transform: translateY(28px);
     background-color: #2da9e2;
     @media (max-width: 490px) {
       width: 1px;
       height: 100%;
-      transform: translateY(0) translateX(6px);
+      transform: translateY(0) translateX(8px);
     }
   }
   &__date {
@@ -125,11 +159,29 @@ export default {
       left: 25px;
     }
   }
+  &__marker {
+    position: absolute;
+    top: 20px;
+    display: inline-block;
+    outline: 2px #225aa5 solid;
+    @media (max-width: 490px) {
+      top: 0;
+      left: 0;
+    }
+  }
   &__item {
     position: relative;
     flex: 1 0 0;
     &:last-child {
       flex: 0;
+    }
+    &:not(&--active) {
+      .timeline__marker {
+        width: 18px;
+        height: 18px;
+        border-radius: 50%;
+        background-color: #2da9e2;
+      }
     }
     @media (max-width: 490px) {
       min-height: 80px;
@@ -137,30 +189,25 @@ export default {
         min-height: 0;
       }
     }
-    &::before {
-      content: "";
-      position: absolute;
-      top: 20px;
-      width: 13px;
-      height: 13px;
-      outline: 2px #225aa5 solid;
-      background-color: #2da9e2;
-      transform: rotate(45deg);
-      @media (max-width: 490px) {
-        top: 0;
-        left: 0;
-      }
-    }
     &--active {
       .timeline__date {
-        color: #add684;
+        color: #fff;
       }
-      &::before {
-        background-color: #add684;
+      .timeline__marker {
+        @include ico-types();
       }
       .timeline__period {
         color: #fff;
       }
+    }
+  }
+  &--cancelled {
+    .timeline__date,
+    .timeline__period {
+      color: #d1d1d1;
+    }
+    .timeline__marker:not(.timeline__marker--cancelled) {
+      background: #d1d1d1;
     }
   }
 }
